101b095e0453d147b96ef79c0dc58d2c112b1940
Harsh sahu <sahuharsh164@gmail.com>
2024-04-18 11:44:44 +0530

    router and controller

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.urlencoded({extended:true, limit:'16kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes
+import userRouter from './routes/user.routes.js'
+
+//rotes declaration
+app.use('/users', userRouter)
+
 export {app}
---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import exp from 'constants';
+ import fs from 'fs'   
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: CLOUDINARY_API_KEY, 
+  api_secret: CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary= async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        //upload the file on cloudinary
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded successfull
+        console.log("file is uploaded successfully",response.url);
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove locally saved temporary file
+        return null;
+    }
+}
+export{uploadOnCloudinary}
